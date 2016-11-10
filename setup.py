354dbfc29df080986162a8ad7856b432b3fd3395
ownport <ownport@gmail.com>
2016-11-10 07:59:40 +0200

    [+] setup.py options

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ from pkgstack import __version__
 setup(
     name=__title__,
     version=__version__,
+    packages=[__title__,],
     url='https://github.com/ownport/pkgstack',
+    author='Andrey Usov',
+    author_email='ownport@gmail.com',
     py_modules=['pkgstack'],
     entry_points='''
         [console_scripts]
-        pkgstack=pkgstack.main:cli
+        pkgstack=pkgstack.main:run
     ''',
+    keywords=['pip', 'install', 'wrapper', 'yaml', 'package', 'pypi']
 )

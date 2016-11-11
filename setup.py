2d4d0d7cb9e43c414aa10ee30e5f8c9d36bd14c3
ownport <ownport@gmail.com>
2016-11-11 23:04:54 +0200

    [+] classifiers

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from pkgstack import __version__
 setup(
     name=__title__,
     version=__version__,
+    description='Simple python package management tool based on pip',
     packages=[__title__,],
     url='https://github.com/ownport/pkgstack',
     author='Andrey Usov',
@@ -15,5 +16,13 @@ setup(
         [console_scripts]
         pkgstack=pkgstack.main:run
     ''',
-    keywords=['pip', 'install', 'wrapper', 'yaml', 'package', 'pypi']
+    keywords=['pip', 'install', 'wrapper', 'yaml', 'package', 'pypi'],
+    classifier=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+    ],
 )
